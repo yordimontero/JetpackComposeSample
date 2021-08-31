8c6510ff837bada55d5dbc914503b32bcaf5cb5d
Yordi Montero <monterocalvoyordi@gmail.com>
2021-08-31 15:49:06 -0600

    ImageCard

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
@@ -21,40 +22,91 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.circleappsstudio.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Testing()
+            val painter = painterResource(id = R.mipmap.header)
+            val description = "This is Jetpack Compose!"
+            val title = "Designing CardView in Jetpack Compose"
+            ImageCard(
+                painter = painter,
+                contentDescription = description,
+                title = title
+            )
         }
     }
 }
 
 @Composable
-private fun Testing() {
-
-    Row(
+private fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Box(
         modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(250.dp)
-        ,
-        horizontalArrangement = Arrangement.SpaceAround,
-        verticalAlignment = Alignment.CenterVertically
+            .fillMaxWidth(0.5f)
+            .padding(16.dp)
     ) {
-        Text(text = "Hello")
-        Text(text = "World!")
-        Text(text = "This is a Text")
-    }
 
+        Card(
+            modifier = modifier.fillMaxWidth(),
+            shape = RoundedCornerShape(15.dp),
+            elevation = 5.dp
+        ) {
+
+            Box(
+                modifier = Modifier.height(200.dp)
+            ) {
+                Image(
+                    painter = painter,
+                    contentDescription = contentDescription,
+                    contentScale = ContentScale.Crop
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(
+                            Brush.verticalGradient(
+                                colors = listOf(
+                                    Color.Transparent,
+                                    Color.Black
+                                ),
+                                startY = 300f
+                            )
+                        )
+                )
+
+                Text(
+                    text = title,
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(16.dp),
+                    style = TextStyle(
+                        color = Color.White,
+                        fontSize = 16.sp
+                    )
+                )
+            }
+
+        }
+
+    }
 }
 
 @Preview(
@@ -63,5 +115,9 @@ private fun Testing() {
 )
 @Composable
 private fun TestingPreview() {
-    Testing()
+    ImageCard(
+        painter = painterResource(id = R.mipmap.header),
+        contentDescription = "This is a description",
+        title = "This is a title"
+    )
 }

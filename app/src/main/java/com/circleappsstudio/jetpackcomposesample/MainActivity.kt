5b2d97a842cdbaa420caf7f81677c91763ec116d
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-03 12:41:37 -0600

    TextField and SnackBar

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -1,83 +1,86 @@
 package com.circleappsstudio.jetpackcomposesample
 
-import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            Column(
-                modifier = Modifier.fillMaxSize()
-            ) {
-
-                val color = remember {
-                    mutableStateOf(
-                        Color.Yellow
-                    )
-                }
-
-                ColorBox(
-                    Modifier
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-                    color.value = it
-                }
-
-                Box(
-                    modifier = Modifier
-                        .background(
-                            color.value
-                        )
-                        .weight(1f)
-                        .fillMaxSize()
-                ) {
-
-                }
-            }
-
+            Test()
         }
     }
 }
 
 @Composable
-private fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit
-) {
+private fun Test() {
 
-    Box(
-        modifier = modifier
-            .background(
-                Color.Red
+    val scaffoldState = rememberScaffoldState()
+
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+
+    val scope = rememberCoroutineScope()
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            OutlinedTextField(
+                value = textFieldState,
+                onValueChange = {
+                    textFieldState = it
+                },
+                label = {
+                    Text(text = "Name")
+                },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
             )
-            .clickable {
-                updateColor(
-                    Color(
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        Random.nextFloat(),
-                        1f
+
+            Spacer(
+                modifier = Modifier.height(16.dp)
+            )
+
+            Button(onClick = {
+
+                scope.launch {
+                    scaffoldState.snackbarHostState.showSnackbar(
+                        "Hi $textFieldState"
                     )
-                )
+                }
+
+            }) {
+                Text(text = "Click me!")
             }
-    )
+
+        }
+
+    }
 
 }
 

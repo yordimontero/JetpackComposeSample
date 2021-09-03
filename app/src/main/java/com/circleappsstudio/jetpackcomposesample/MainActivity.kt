c7dfeaaa477af5d9ee32edbd2135f77a04951ea4
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-03 15:54:58 -0600

    ConstraintLayout

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
@@ -24,6 +25,10 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.constraintlayout.compose.ChainStyle
+import androidx.constraintlayout.compose.ConstraintLayout
+import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import kotlinx.coroutines.launch
 import kotlin.random.Random
 
@@ -36,64 +41,53 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class User(
-    val name: String = "",
-    val age: Int = 0
-)
-
-val userList = listOf(
-    User(
-        "Juan",
-        25
-    ),
-    User(
-        "Luis",
-        19
-    ),
-    User(
-        "María",
-        32
-    )
-)
-
 @Composable
 private fun Test() {
 
-    LazyColumn {
+    val constraints = ConstraintSet {
 
-        itemsIndexed(
-            userList
-        ) { index, item ->
+        val greenBox = createRefFor("greenbox")
+        val redBox = createRefFor("redbox")
 
-            Text(
-                text = "#$index: ${item.name}, ${item.age}",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-                    .background(Color.DarkGray),
-                color = Color.White
-            )
+        val guideline = createGuidelineFromTop(0.5f)
 
+        constrain(greenBox) {
+            //top.linkTo(parent.top)
+            top.linkTo(guideline)
+            start.linkTo(parent.start)
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
         }
 
-        /*items(5000) {
+        constrain(redBox) {
+            top.linkTo(parent.top)
+            start.linkTo(greenBox.end)
+            end.linkTo(parent.end)
+            //width = Dimension.fillToConstraints
+            width = Dimension.value(100.dp)
+            height = Dimension.value(100.dp)
+        }
 
-            Text(
-                text = "Item: $it",
-                fontSize = 30.sp,
-                fontWeight = FontWeight.Bold,
-                textAlign = TextAlign.Center,
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 24.dp)
-                    .background(Color.DarkGray),
-                color = Color.White
-            )
+        createHorizontalChain(greenBox, redBox, chainStyle = ChainStyle.Packed)
 
-        }*/
+    }
+
+    ConstraintLayout(
+        constraintSet = constraints,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Box(
+            modifier = Modifier
+                .background(Color.Green)
+                .layoutId("greenbox")
+        )
+
+        Box(
+            modifier = Modifier
+                .background(Color.Red)
+                .layoutId("redbox")
+        )
 
     }
 

ca68a26f60facecbc69276f3d8b83d37a809e7f9
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-06 15:39:45 -0600

    ProgressBar

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -7,26 +7,25 @@ import androidx.activity.OnBackPressedDispatcher
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.layoutId
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.constraintlayout.compose.ChainStyle
@@ -40,73 +39,71 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Test()
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Test(percentage = 0.19f, number = 100)
+            }
         }
     }
 }
 
 @Composable
-private fun Test() {
+private fun Test(
+    percentage: Float,
+    number: Int,
+    fontSize: TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color: Color = Color.Green,
+    strokeWidth: Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+) {
 
-    var sizeState by remember {
-        mutableStateOf(200.dp)
+    var animationPlayed by remember {
+        mutableStateOf(false)
     }
 
-    val size by animateDpAsState(
-        targetValue = sizeState,
-
-        // Slowly animation:
-        /*animationSpec = tween(
-            durationMillis = 3000,
-            delayMillis = 300,
-            easing = FastOutLinearInEasing
-        )*/
-
-        // Bouncy animation:
-        /*spring(
-            Spring.DampingRatioHighBouncy
-        )*/
-
-        // Custom animation:
-        /*keyframes {
-            durationMillis = 5000
-            sizeState at 0 with LinearEasing
-            sizeState * 1.5f at 1000 with FastOutLinearInEasing
-            sizeState * 2f at 5000
-        }*/
-
-    )
-
-    // Animation color:
-    val infiniteTransition = rememberInfiniteTransition()
-
-    val color = infiniteTransition.animateColor(
-        initialValue = Color.Red,
-        targetValue = Color.Green,
-        animationSpec = infiniteRepeatable(
-            animation = tween(
-                durationMillis = 2000
-            ),
-            repeatMode = RepeatMode.Reverse
+    val currentPercent = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
         )
     )
 
+    LaunchedEffect(key1 = true) {
+        animationPlayed = true
+    }
+
     Box(
-        modifier = Modifier
-            .size(size)
-            .background(color.value),
+        modifier = Modifier.size(radius * 2f),
         contentAlignment = Alignment.Center
     ) {
-        Button(
-            onClick = {
-                sizeState += 50.dp
-            }
+
+        Canvas(
+            modifier = Modifier.size(radius * 2f)
         ) {
-            Text(
-                text = "Increase Size",
-                color = color.value
+            drawArc(
+                color = color,
+                startAngle = -90f,
+                sweepAngle = 360 * currentPercent.value,
+                useCenter = false,
+                style = Stroke(
+                    width = strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
             )
         }
+
+        Text(
+            text = (currentPercent.value * number).toInt().toString(),
+            color = Color.Black,
+            fontSize = fontSize,
+            fontWeight = FontWeight.Bold
+        )
+
     }
 
 }

2790c56670f6cacf125b8c1b2da0d94ae4d76f90
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-07 17:42:45 -0600

    Meditation UI - Greeting Section, ChipSelection and Current Meditation

---
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/ui/HomeScreen.kt
@@ -0,0 +1,195 @@
+package com.circleappsstudio.jetpackcomposesample.ui
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.circleappsstudio.jetpackcomposesample.R
+import com.circleappsstudio.jetpackcomposesample.ui.theme.*
+
+@Composable
+fun HomeScreen() {
+
+    Box(
+        modifier = Modifier
+            .background(DeepBlue)
+            .fillMaxSize()
+    ) {
+        Column {
+            GreetingSection()
+            ChipSection(
+                chips = listOf(
+                    "Sweet Sleep",
+                    "Insomnia",
+                    "Depression"
+                )
+            )
+            CurrentMeditation()
+        }
+    }
+
+}
+
+@Composable
+fun GreetingSection(name: String = "Yordi") {
+
+    val searchIcon = painterResource(id = R.drawable.ic_search)
+
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(15.dp),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Text(
+                text = "Good afternoon, $name!",
+                style = MaterialTheme.typography.h2
+            )
+
+            Text(
+                text = "We wish you have a good day.",
+                style = MaterialTheme.typography.body1
+            )
+
+        }
+
+        Icon(
+            painter = searchIcon,
+            contentDescription = "Search",
+            tint = Color.White,
+            modifier = Modifier
+                .size(24.dp)
+        )
+
+    }
+
+}
+
+@Composable
+fun ChipSection(
+    chips: List<String>
+) {
+
+    var selectedChipState by remember {
+        mutableStateOf(0)
+    }
+
+    LazyRow {
+        items(count = chips.size) {
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .padding(
+                        start = 15.dp,
+                        top = 15.dp,
+                        bottom = 15.dp
+                    )
+                    .clickable {
+                        selectedChipState = it
+                    }
+                    .clip(
+                        RoundedCornerShape(10.dp)
+                    )
+                    .background(
+                        if (selectedChipState == it) {
+                            ButtonBlue
+                        } else {
+                            DarkerButtonBlue
+                        }
+                    )
+                    .padding(15.dp)
+            ) {
+
+                Text(
+                    text = chips[it],
+                    color = TextWhite
+                )
+
+            }
+
+        }
+    }
+
+}
+
+@Composable
+fun CurrentMeditation(
+    color: Color = LightRed
+) {
+
+    val playIcon = painterResource(id = R.drawable.ic_play)
+
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .padding(15.dp)
+            .clip(
+                RoundedCornerShape(10.dp)
+            )
+            .background(color = color)
+            .padding(
+                horizontal = 15.dp,
+                vertical = 20.dp
+            )
+            .fillMaxWidth()
+    ) {
+
+        Column {
+
+            Text(
+                text = "Daily Thought",
+                style = MaterialTheme.typography.h2
+            )
+
+            Text(
+                text = "Meditation • 3-10 min",
+                style = MaterialTheme.typography.body1,
+                color = TextWhite
+            )
+
+        }
+        
+        Box(
+            contentAlignment = Alignment.Center,
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .background(ButtonBlue)
+                .padding(10.dp)
+        ) {
+            Icon(
+                painter = playIcon,
+                contentDescription = "Play",
+                tint = Color.White,
+                modifier = Modifier.size(16.dp)
+            )
+        }
+        
+    }
+
+}

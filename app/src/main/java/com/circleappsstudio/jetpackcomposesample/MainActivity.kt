9fd2e3caa61860bc64d62677e8dba85f78b58ea4
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-14 17:17:49 -0600

    Timer

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -1,48 +1,210 @@
 package com.circleappsstudio.jetpackcomposesample
 
 import android.os.Bundle
-import android.view.MotionEvent
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.rotate
-import androidx.compose.ui.focus.focusModifier
-import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.input.pointer.pointerInteropFilter
-import androidx.compose.ui.layout.boundsInWindow
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.PointMode
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
-import com.circleappsstudio.jetpackcomposesample.ui.HomeScreen
+import androidx.compose.ui.unit.sp
 import com.circleappsstudio.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
-import kotlin.math.PI
-import kotlin.math.atan2
-import kotlin.math.roundToInt
+import com.circleappsstudio.jetpackcomposesample.ui.theme.LightGreen3
+import com.circleappsstudio.jetpackcomposesample.ui.theme.LightRed
+import kotlinx.coroutines.delay
+import kotlin.math.*
 
 class MainActivity : ComponentActivity() {
 
-    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackComposeSampleTheme {
-                HomeScreen()
+
+                Surface(
+                    color = Color(0xFF212121),
+                    modifier = Modifier.fillMaxSize()
+                ) {
+                    Box(
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Timer(
+                            totalTime = 60L * 1000L,
+                            handleColor = Color.Green,
+                            inactiveBarColor = Color.DarkGray,
+                            activeBarColor = Color(0xFF37B900),
+                            modifier = Modifier.size(200.dp)
+                        )
+                    }
+                }
+
             }
         }
     }
 }
 
+@Composable
+fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    inactiveBarColor: Color,
+    activeBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 1f,
+    strokeWidth: Dp = 5.dp
+) {
 
+    var size by remember {
+        mutableStateOf(IntSize.Zero)
+    }
+
+    var percentValue by remember {
+        mutableStateOf(initialValue)
+    }
+
+    var currentTime by remember {
+        mutableStateOf(totalTime)
+    }
+
+    var isTimerRunning by remember {
+        mutableStateOf(false)
+    }
+
+    LaunchedEffect(
+        key1 = currentTime,
+        key2 = isTimerRunning
+    ) {
+
+        if (currentTime > 0 && isTimerRunning) {
+            delay(100L)
+            currentTime -= 100L
+            percentValue = currentTime / totalTime.toFloat()
+        }
+
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .onSizeChanged {
+                size = it
+            }
+    ) {
+
+        Canvas(
+            modifier = modifier
+        ) {
+
+            drawArc(
+                color = inactiveBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f,
+                useCenter = false,
+                size = Size(
+                    size.width.toFloat(),
+                    size.height.toFloat()
+                ),
+                style = Stroke(
+                    width = strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
+            )
+
+            drawArc(
+                color = activeBarColor,
+                startAngle = -215f,
+                sweepAngle = 250f * percentValue,
+                useCenter = false,
+                size = Size(
+                    size.width.toFloat(),
+                    size.height.toFloat()
+                ),
+                style = Stroke(
+                    width = strokeWidth.toPx(),
+                    cap = StrokeCap.Round
+                )
+            )
+
+            val center = Offset(
+                x = size.width / 2f,
+                y = size.height / 2f
+            )
+
+            val beta = (250f * percentValue + 145f) * (PI / 180f).toFloat()
+
+            val radius = size.width / 2f
+            val a = cos(beta) * radius
+            val b = sin(beta) * radius
+
+            drawPoints(
+                points = listOf(
+                    Offset(
+                        center.x + a,
+                        center.y + b
+                    )
+                ),
+                pointMode = PointMode.Points,
+                color = handleColor,
+                strokeWidth = (strokeWidth * 3f).toPx(),
+                cap = StrokeCap.Round
+            )
+
+        }
+
+        Text(
+            text = (currentTime / 1000L).toString(),
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.White
+        )
+
+        Button(
+            onClick = {
+                      if (currentTime <= 0L) {
+                          currentTime = totalTime
+                          isTimerRunning = true
+                      } else {
+                          isTimerRunning = !isTimerRunning
+                      }
+            },
+            modifier = Modifier.align(
+                Alignment.BottomCenter
+            ),
+            colors = ButtonDefaults.buttonColors(
+                backgroundColor = if (!isTimerRunning || currentTime <= 0L)
+                    LightGreen3
+                else
+                    LightRed
+            )
+        ) {
+            Text(
+                text = if (isTimerRunning && currentTime >= 0L) "Stop"
+                else if (!isTimerRunning && currentTime >= 0L) "Start"
+                else "Restart",
+                color = Color.Black
+            )
+        }
+
+    }
+
+}
 
 @Preview(
     showSystemUi = true,

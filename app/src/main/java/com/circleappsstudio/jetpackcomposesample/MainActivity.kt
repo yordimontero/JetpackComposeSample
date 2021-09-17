30d7a4ed37144d088d872d7048549d720e0e3c00
Yordi Montero <monterocalvoyordi@gmail.com>
2021-09-17 15:55:43 -0600

    SplashScreen

--- app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
+++ app/src/main/java/com/circleappsstudio/jetpackcomposesample/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.circleappsstudio.jetpackcomposesample
 
 import android.os.Bundle
+import android.view.animation.OvershootInterpolator
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Animatable
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.*
@@ -20,6 +22,7 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,6 +30,10 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.circleappsstudio.jetpackcomposesample.ui.*
 import com.circleappsstudio.jetpackcomposesample.ui.theme.JetpackComposeSampleTheme
 import com.circleappsstudio.jetpackcomposesample.ui.theme.LightGreen3
@@ -39,8 +46,91 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Navigation()
+            Surface(
+                color = Color(0xFF212121),
+                modifier = Modifier.fillMaxSize()
+            ) {
+                MainNavigation()
+            }
         }
+    }
+
+}
+
+
+@Composable
+fun MainNavigation() {
+
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash_screen"
+    ) {
+
+        composable(route = "splash_screen") {
+            SplashScreenTest(navController = navController)
+        }
+
+        composable(route = "main_screen") {
+            MainScreenTest()
+        }
+
+    }
+
+}
+
+@Composable
+fun SplashScreenTest(navController: NavController) {
+
+    val scale = remember {
+        Animatable(0f)
+    }
+
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate(route = "main_screen") {
+            popUpTo("splash_screen") {
+                inclusive = true
+            }
+        }
+    }
+
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+
+        Image(
+            painter = painterResource(id = R.drawable.hong_kong),
+            contentDescription = "Icon",
+            modifier = Modifier.scale(scale.value)
+        )
+
+    }
+    
+}
+
+@Composable
+fun MainScreenTest() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "Hi!",
+            textAlign = TextAlign.Center,
+            color = Color.White
+        )
     }
 }
 
